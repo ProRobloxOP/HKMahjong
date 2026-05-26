40e8a4c167c4973087355b10339c8ae6c840e988
ProRobloxOP <prorobloxop.madala@gmail.com>
2026-05-25 22:00:57 -0400

    Eh

---
+++ Assets/Scripts/Systems/GUI/WelcomeScreen.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEditor.DeviceSimulation;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class WelcomeScreen : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+     // Update is called once per frame
+     void Update()
+    {
+       
+    }
+
+     public void OnPointerClick(PointerEventData eventData)
+     {
+           gameObject.SetActive(false);
+     }
+}
