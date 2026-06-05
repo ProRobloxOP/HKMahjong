00b56e9b557b78fc800115f2a30e8d608034a96c
BT_C09_10 <mobileappdev@bths.edu>
2026-06-05 11:03:56 -0400

    pain

--- Assets/Scripts/Systems/GUI/WelcomeScreen.cs
+++ Assets/Scripts/Systems/GUI/WelcomeScreen.cs
@@ -2,9 +2,11 @@
 
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class WelcomeScreen : MonoBehaviour, IPointerClickHandler
 {
+      public GameObject buttonsObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
       {
@@ -20,6 +22,7 @@ public class WelcomeScreen : MonoBehaviour, IPointerClickHandler
       public void OnPointerClick(PointerEventData eventData)
       {
             gameObject.SetActive(false);
+            buttonsObject.SetActive(true);
       }
 
 }
