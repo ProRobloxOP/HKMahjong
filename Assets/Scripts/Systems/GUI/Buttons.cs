ec34b4f2be14d2709a0fc3ad61aa56c652b44f67
BT_C09_10 <mobileappdev@bths.edu>
2026-06-05 15:35:19 -0400

    trying to start scripting button events

--- Assets/Scripts/Systems/GUI/Buttons.cs
+++ Assets/Scripts/Systems/GUI/Buttons.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Buttons : MonoBehaviour
 {
+    public Button buttonChow;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,18 @@ public class Buttons : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Pung(Tile tile)
+    {
+        // for (int i = 0; i < tileHand.length; i++)
+        // {
+        //     if (tileHand(i) == tile) {
+        //     for (int j = i; j < tileHand.length; j++)
+        // {
+            
+        // }
+        // }
+        // }
     }
 }
