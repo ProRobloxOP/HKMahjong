f5e53bed0aa6d6ba85150fc43aad6d882ddd7996
BT_C09_10 <mobileappdev@bths.edu>
2026-06-05 15:26:38 -0400

    kevin what are u doing cuh

--- Assets/Settings/HDRP.cs
+++
@@ -1,8 +0,0 @@
-        static bool CanShowWizard()
-        {
-            // If the user has more than one SRP installed, only show the Wizard if the pipeline is HDRP
-            return HDRenderPipeline.isReady;
-            return HDRenderPipeline.isReady || RenderPipelineManager.currentPipeline == null;
-        }
-
-        void OnGUI();
