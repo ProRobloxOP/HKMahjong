68259b25d138fa798a82693e3344b2147c560b3b
Waylon <thatwaylonli@gmail.com>
2026-06-14 01:25:20 -0400

    one day...

--- Assets/Scripts/Systems/MatchSystem/Buttons.cs
+++ Assets/Scripts/Systems/MatchSystem/Buttons.cs
@@ -1,8 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Mono.Cecil;
+using Unity.Burst;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.Rendering;
 
 public class Buttons : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+   private Dictionary<string, List<Tile>> hand = PlayerHand.hand;
+    private List<string> tileList = HandGUI.tileList;
     void Start()
     {
         
