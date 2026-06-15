20965bcdd091341b08fdaa05090b42a6ea043cc8
ProRobloxOP <prorobloxop.madala@gmail.com>
2026-06-15 07:26:07 -0400

    Animated Drops

---
+++ Assets/Scripts/Systems/MatchSystem/RoundLogic.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class RoundLogic : MonoBehaviour
+{
+    public static event Action<int> DrawTile;
+    int currPlayer = 0;
+
+    private void OnEnable()
+     {
+         PlayerHand.PlayerDroppedTile += SwitchPlayerTurn; 
+         WelcomeScreen.StartRound += StartRound;
+     } 
+    private void OnDisable()
+     {
+          PlayerHand.PlayerDroppedTile -= SwitchPlayerTurn;
+          WelcomeScreen.StartRound -= StartRound;
+     } 
+
+    private async void SwitchPlayerTurn(int lastPlayer, Tile droppedTile)
+     {
+          currPlayer = (currPlayer == 4)? 1 : currPlayer + 1;
+          await Task.Delay(300);
+          DrawTile?.Invoke(currPlayer);
+     }
+
+     private void StartRound()
+     {
+          SwitchPlayerTurn(0, null);
+     }
+
+     
+    void Start()
+     {
+          
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
