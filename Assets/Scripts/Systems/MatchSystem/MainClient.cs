3903a7d814fde80d295cecc91c018c317ea90e9d
ProRobloxOP <prorobloxop.madala@gmail.com>
2026-06-15 14:55:01 -0400

    Updated

--- Assets/Scripts/Systems/MatchSystem/MainClient.cs
+++ Assets/Scripts/Systems/MatchSystem/MainClient.cs
@@ -31,11 +31,6 @@ public class MainClient : MonoBehaviour
 
     private PlayerHand clientHand;
 
-    private void OnTileDrop(int playerIndex, Tile tile)
-    {
-        refreshHandList(); 
-    }
-
     private void SetupClientHand()
     {
         GameObject[] rootObjs = SceneManager.GetActiveScene().GetRootGameObjects();
@@ -79,71 +74,15 @@ public class MainClient : MonoBehaviour
         DropTile();
     }
 
-    private IEnumerator RefreshNextFrame()
-    {
-        yield return null; // wait one frame for UIDocument to finish initializing
-        refreshHandList();
-    }
-
-    private void refreshHandList()
-    {
-        rootElement = tileUI.rootVisualElement;
-        PrintHierarchy(rootElement);
-
-        ScrollView handScroll = rootElement.Q<ScrollView>("Hand");
-        handScroll.contentContainer.style.flexDirection = FlexDirection.Row;
-        handScroll.contentContainer.style.flexWrap = Wrap.NoWrap;
-        handScroll.mode = ScrollViewMode.Horizontal;
-        handScroll.horizontalScrollerVisibility = ScrollerVisibility.Hidden;
-
-        handScroll.Clear();
-
-        List<Tile> allTiles = clientHand.tiles.Values.SelectMany(t => t).ToList();
-
-        foreach (Tile tile in allTiles)
-        {
-            VisualElement wrapper = new VisualElement();
-            tileAsset.CloneTree(wrapper);
-            VisualElement tileEl = wrapper.Q<VisualElement>("Tile");
-            if (tileEl == null) continue;
-
-            Sprite background = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Images/HKTiles/" + tile.ToString() + ".png");
-            if (background != null)
-                tileEl.style.backgroundImage = new StyleBackground(background);
-
-            // capture tile in local variable for the lambda
-            Tile capturedTile = tile;
-            tileEl.RegisterCallback<PointerDownEvent>(evt =>
-            {
-                if (evt.clickCount == 2)
-                {
-                    clientHand.DropTile(clientHand.playerIndex, capturedTile);
-                    clientHand.DrawTilesFromWall(1); // draw next tile
-                    refreshHandList();
-                }
-            });
-            handScroll.Add(wrapper);
-        }
-    }
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        clientHand = PlayerHand.CreateInstance<PlayerHand>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    private void PrintHierarchy(VisualElement element, int depth = 0)
-    {
-        Debug.Log($"{new string('-', depth)}{element.GetType().Name} name='{element.name}' class='{string.Join(",", element.GetClasses())}'");
-        foreach (var child in element.Children())
-        {
-            PrintHierarchy(child, depth + 1);
-        }
     }
 }
